01e3b6c2da9ade3e7fb0c5b588f294abde798e86
Farbod <Farboddaneshmandfard@gmail.com>
2025-12-28 16:46:20 +0330

    fixing build,deploy,eslint

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,60 +1,64 @@
-import React ,{useState} from 'react';
+import React, { useMemo, useState } from 'react';
 import './App.css';
-import Products from './Products.json'
-import Product from './../Products/products';
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from 'react-router-dom';
+import Products from './Products.json';
+import Product from '../Products/products';
 import Layout from '../Layout/Layout';
 import About from '../About/About';
 import Contact from '../Contact/Contact';
 import Blog from '../Blog/Blog';
 import ThemeContext from '../../Contexts/Themecontext';
-import themeconfig from "../../Configs/theme";
+import themeconfig from '../../Configs/theme';
 import Singleproducts from '../Singleproduct/Singleproducts';
-import{
-  BrowserRouter,
-  Route,
-  Routes,
-} from "react-router-dom";
 
 function App() {
-  const [activetheme,setactivetheme] = useState('green')
+  const [activetheme, setactivetheme] = useState('green');
+  const contectvalue = useMemo(() => ({
+    theme: themeconfig[activetheme],
+    setactivetheme,
+  }), [activetheme]);
   return (
-    <ThemeContext.Provider value={
-        {
-          theme: themeconfig[activetheme],
-          setactivetheme
-        }
-      }>
-      <div className='App'>
-        <BrowserRouter>
+    <ThemeContext.Provider value={contectvalue}>
+      <div className="App">
+        <BrowserRouter basename="/React-learn">
           <Layout>
             <Routes>
-              <Route path='/' element={
-                <>
-                    <Product 
+              <Route
+                path="/"
+                element={(
+                  <Product
                     info={Products}
                   />
-                </>
-              } />
-              <Route path='/about' element={
-                <>
-                    <About/>
-                </>
-              }/> 
-              <Route path='/contact' element={
-                <>
-                    <Contact/>
-                </>
-              }/> 
-              <Route path='/Blog' element={
-                <>
-                    <Blog/>
-                </>
-              }/> 
-              <Route path='/product/:id' element={
-                <>
-                    <Singleproducts/>
-                </>
-              }/> 
+              )}
+              />
+              <Route
+                path="/about"
+                element={(
+                  <About />
+              )}
+              />
+              <Route
+                path="/contact"
+                element={(
+                  <Contact />
+              )}
+              />
+              <Route
+                path="/Blog"
+                element={(
+                  <Blog />
+              )}
+              />
+              <Route
+                path="/product/:id"
+                element={(
+                  <Singleproducts />
+              )}
+              />
             </Routes>
           </Layout>
         </BrowserRouter>
--- src/components/Productitems/productsitems.js
+++ src/components/Productitems/productsitems.js
@@ -1,77 +1,84 @@
-import React,{useContext, useEffect, useRef} from "react";
-import Button from "../BTN/Button";
-import Image from "../image/image";
-import './productsitems.css'
-import ThemeContext from "../../Contexts/Themecontext";
-import cartcontext from "../../Contexts/cartcontext";
-import { MdAdd, MdOutlineRemoveShoppingCart} from "react-icons/md";
-import { Link } from "react-router-dom";
-import { PROJECT_URL } from "../../Configs/general";
+import React, { useContext, useEffect, useRef } from 'react';
+import Button from '../BTN/Button';
+import Image from '../image/image';
+import './productsitems.css';
+import ThemeContext from '../../Contexts/Themecontext';
+import cartcontext from '../../Contexts/cartcontext';
+import { MdAdd, MdOutlineRemoveShoppingCart } from 'react-icons/md';
+import { Link } from 'react-router-dom';
+import { PROJECT_URL } from '../../Configs/general';
 // import productreducer from "../../reducers/Productitems.reducer";
 
-
-
-const Productsitem=({data})=>{
-  const themeValue=useContext(ThemeContext)
-  const {carts,dispatchcart}=useContext(cartcontext)
-  const productref=useRef(null)
+function Productsitem({ data }) {
+  const themeValue = useContext(ThemeContext);
+  const { carts, dispatchcart } = useContext(cartcontext);
+  const productref = useRef(null);
   // const [added,setdded]=useState(false)
   // const [state,dispatch]=useReducer(productreducer,{added:false})
   // console.log('themeValue: ',themeValue);
-  const cartadded = carts.includes(data.id)
-  const handleadd=()=>{
+  const cartadded = carts.includes(data.id);
+  const handleadd = () => {
     // setdded(!added)
     if (cartadded) {
     //   dispatch({
     //   type:'Removed_From_Cart'
     // })
       dispatchcart({
-      type:'Removed_From_Cart',
-      id:data.id
-    })
+        type: 'Removed_From_Cart',
+        id: data.id,
+      });
     } else {
     //   dispatch({
     //   type:'Added_To_Cart'
     // })
       dispatchcart({
-      type:'Added_To_Cart',
-      id:data.id
-    })
+        type: 'Added_To_Cart',
+        id: data.id,
+      });
     }
-  }
+  };
   // console.log('state: ',state);
-  
-useEffect(()=>{
-  productref.current.classList.add('visible')
-},[])
+
+  useEffect(() => {
+    productref.current.classList.add('visible');
+  }, []);
 
   return (
-        <li className="productitem" ref={productref}>
-          <Link to={`/product/${data.id}`}>
-            <h4>{data.name}</h4>
-            <Image source={`${PROJECT_URL}/assets/img/${data.image}`}/>
-          </Link>
-            <span>
-              Price: {data.price}
-            </span>
-            <Button  
-            handleclick={handleadd}
-            style={
-              {color:themeValue.theme.color,
-                borderColor:themeValue.theme.color,
+    <li className="productitem" ref={productref}>
+      <Link to={`/product/${data.id}`}>
+        <h4>{data.name}</h4>
+        <Image source={data.image} />
+        {/* <Image source={`${PROJECT_URL}/assets/img/${data.image}`} /> */}
+      </Link>
+      <span>
+        Price:
+        {' '}
+        {data.price}
+      </span>
+      <Button
+        handleclick={handleadd}
+        style={
+              {
+                color: themeValue.theme.color,
+                borderColor: themeValue.theme.color,
               }
-            }>
-              {cartadded ? (<>
-                <MdOutlineRemoveShoppingCart />
-                Remove
-              </>)
-              : 
-              (<>
-                <MdAdd />
-                Add to Cart
-              </>)}
-            </Button>
-        </li>
-)}
+            }
+      >
+        {cartadded ? (
+          <>
+            <MdOutlineRemoveShoppingCart />
+            Remove
+          </>
+        )
+          : (
+            <>
+              <MdAdd />
+              Add to Cart
+            </>
+          )}
+      </Button>
+    </li>
+  );
+}
 
-export default Productsitem
+export default Productsitem;
